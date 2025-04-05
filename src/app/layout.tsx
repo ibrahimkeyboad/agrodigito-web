928a917d8a4b274c2d32172f101efd88afa5ae9d
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-04-05 17:30:37 +0300

    Add type annotation for metadata in layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title:
     'Agrovet  - Connecting Agrovets with Trusted Agricultural Input Suppliers',
   description:
