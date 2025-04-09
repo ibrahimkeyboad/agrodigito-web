000e78591ccfc037722b69124ecb1bdc478dd331
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-04-09 15:58:12 +0300

    Add meta tags and Open Graph data to enhance SEO and social media sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,47 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <header>
+        <meta
+          name='keywords'
+          content='agrilink, agriculture, agrovet, farming, fertilizers, pesticides, crop protection, agricultural supplies, farm inputs'
+        />
+
+        {/* <Open Graph / Facebook  */}
+        <meta
+          property='og:title'
+          content='AgriLink - Connecting Agrovets with Agricultural Input Suppliers'
+        />
+        <meta
+          property='og:description'
+          content='Get real-time access to fertilizers, pesticides, insecticides, fungicides, seeds, and other essential agricultural products, all in one place.'
+        />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://agrilink.com/' />
+        <meta
+          property='og:image'
+          content='https://lovable.dev/opengraph-image-p98pqg.png'
+        />
+
+        {/* Twitter  */}
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:site' content='@agrilink' />
+        <meta
+          name='twitter:title'
+          content='AgriLink - Connecting Farmers with Suppliers'
+        />
+        <meta
+          name='twitter:description'
+          content='Get real-time access to essential agricultural products all in one place.'
+        />
+        <meta
+          name='twitter:image'
+          content='https://lovable.dev/opengraph-image-p98pqg.png'
+        />
+
+        {/* Canonical URL */}
+        <link rel='canonical' href='https://agrilink.com/' key='canonical' />
+      </header>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
