b68ab1e61cf38a4023039dfb8b9766bb86129184
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-04-09 16:37:18 +0300

    Add structured data component to enhance SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,13 @@ import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
 import HowItWorksSection from '@/components/HowItWorkSection';
 import ProductCategoriesSection from '@/components/ProductSection';
+import StructuredData from '@/components/structured-data';
 
 export default function Home() {
   return (
     <div className='flex min-h-screen flex-col bg-white'>
+      <StructuredData />
+
       <Header />
       {/* <Navbar /> */}
       <HeroSection />
