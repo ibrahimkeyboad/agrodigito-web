9eff3b4fcf1b1eab2fac16f818f597db273daf8f
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-08-29 07:22:34 +0300

    Add metadata for AgroDigito with title, description, and keywords

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,14 @@ import HowItWorksSection from '@/components/HowItWorkSection';
 import ProductCategoriesSection from '@/components/ProductSection';
 import StructuredData from '@/components/structured-data';
 
+export const metadata = {
+  title: 'AgroDigito - Connecting Agrovets with Trusted Suppliers',
+  description:
+    'Platform for seamless ordering of agricultural inputs like seeds, fertilizers, and pesticides with real-time insights.',
+  keywords:
+    'agrodigito, agrovets, agricultural inputs, suppliers, farming supplies, agriculture, seeds, fertilizers, pesticides, ',
+};
+
 export default function Home() {
   return (
     <div className='flex min-h-screen flex-col bg-white'>
