be33d84edabe00f11647e59528ecfaa39981f24e
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-08-29 07:22:24 +0300

    Remove commented-out Agrovet branding from Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,12 +24,7 @@ function Header() {
   return (
     <header className='sticky top-0 z-50 w-full border-b bg-white/95 backdrop-blur supports-[backdrop-filter]:bg-white/60 shadow-sm'>
       <div className='container flex h-16 items-center justify-between'>
-        {/* <div className='flex gap-6 md:gap-10'>
-          <Link href='/' className='flex items-center space-x-2'>
-            <Leaf className='h-6 w-6 text-green-600' />
-            <span className='inline-block font-bold text-xl'>Agrovet </span>
-          </Link>
-        </div> */}
+      
 
         <div className='flex items-center'>
           <Link href='/' className='flex items-center space-x-2'>
