2e8461594bb5cee0720758c152848c1730afdfc8
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-08-29 07:23:20 +0300

    Update contact phone number format in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -178,7 +178,7 @@ function Footer() {
                   className='h-5 w-5 text-green-600 mt-0.5'>
                   <path d='M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z' />
                 </svg>
-                <span className='text-white'>+255763348756</span>
+                <span className='text-white'>+255 746 431 903</span>
               </li>
               <li className='flex items-start space-x-2'>
                 <svg
